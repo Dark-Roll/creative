d26afa47be35030e723a1f156ceb13dad52aa585
Shiou <ai791016ai791016@gmail.com>
2018-05-22 00:18:58 +0800

    add: promise

---
+++ src/promise.js
@@ -0,0 +1,28 @@
+let delayTime = 0
+
+const delay= (callback, time)=> new Promise(resolve=>{
+    setTimeout(() => {
+        callback()
+        resolve(++delayTime)
+    }, time);
+})
+
+delay(()=>console.log('我是帥哥'),1000)
+    .then((time)=>{
+        return delay(
+            ()=>console.log('我是修哥' + time),
+            500
+        )
+    })
+    .then((time)=>{
+        return delay(
+            ()=>console.log('我是修哥' + time),
+            500
+        )
+    })
+    .then((time)=>{
+        return delay(
+            ()=>console.log('我是修哥' + time),
+            500
+        )
+    })
